0a60e3dbbc985943b38b001df8c99e820f3c6f5c
Julien Hauseux <julien.hauseux@gmail.com>
2024-12-09 18:06:58 +0100

    chore(vite): update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    target: "esnext",
+    target: "es2020",
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
   },
   plugins: [
     vue({
