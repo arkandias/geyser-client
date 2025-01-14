0223e0fc12d33f2ab19833de0574527c4b2b3e57
Julien Hauseux <julien.hauseux@gmail.com>
2025-01-14 02:56:41 +0100

    WIP

--- codegen.ts
+++ codegen.ts
@@ -1,8 +1,27 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.development" });
+dotenv.config({ path: ".env.development.local" });
+
+const GRAPHQL_URL = process.env["VITE_GRAPHQL_URL"];
+const HASURA_ADMIN_SECRET = process.env["VITE_HASURA_ADMIN_SECRET"];
+if (!GRAPHQL_URL) {
+  throw new Error("VITE_GRAPHQL_URL environment variable is required");
+}
+if (!HASURA_ADMIN_SECRET) {
+  throw new Error("VITE_HASURA_ADMIN_SECRET environment variable is required");
+}
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "schema.graphql",
+  schema: {
+    [GRAPHQL_URL]: {
+      headers: {
+        "X-Hasura-Admin-Secret": HASURA_ADMIN_SECRET,
+      },
+    },
+  },
   documents: [
     "src/**/*.graphql",
     "src/**/*.ts",
@@ -19,6 +38,9 @@ const config: CodegenConfig = {
       config: {
         useTypeImports: true,
       },
+    },
+    "schema.graphql": {
+      plugins: ["schema-ast"],
     },
   },
 };
