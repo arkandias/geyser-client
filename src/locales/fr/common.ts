f9da8df570f955fec0f9a7b06b459523b61b1ba6
Julien Hauseux <julien.hauseux@gmail.com>
2025-03-06 00:17:23 +0100

    WIP

--- src/locales/fr/common.ts
+++ src/locales/fr/common.ts
@@ -29,4 +29,67 @@ export default {
     hours: "h",
     weightedHours: "htd",
   },
+  editor: {
+    button: {
+      save: "Enregistrer",
+      cancel: "Annuler",
+    },
+    save: {
+      noChanges: "Pas de changement à enregistrer",
+      success: {
+        updated: "Texte mis à jour",
+        deleted: "Texte supprimé",
+      },
+      error: {
+        update: "Échec de la mise à jour",
+        delete: "Échec de la suppression",
+      },
+    },
+  },
+  requestCard: {
+    group: "groupe | groupes",
+    hour: "heure | heures",
+    tooltip: {
+      assign: "Attribuer la demande",
+      remove: "Supprimer la demande",
+    },
+  },
+  requestForm: {
+    invalid: {
+      message: "Formulaire non valide",
+      caption: {
+        noTeacher: "Sélectionnez un intervenant",
+        negativeHours: "Sélectionnez un nombre d'heures positif ou nul",
+        noType: "Sélectionnez un type de demande",
+      },
+    },
+    tooltip: {
+      submit: "Valider la demande",
+    },
+  },
+  request: {
+    type: {
+      assignment: "Attribution",
+      primary: "Demande principale",
+      secondary: "Demande secondaire",
+    },
+    error: {
+      courseNotFound: "Erreur lors de la récupération du cours",
+      serviceNotFound: {
+        title: "Pas de service trouvé",
+        caption: "Veuillez d'abord créer un service",
+      },
+      fetch: "Erreur lors de la récupération de la demande actuelle",
+      invalidType: "Type de requête invalide",
+      create: "Échec de la création",
+      update: "Échec de la mise à jour",
+      delete: "Échec de la suppression",
+    },
+    success: {
+      alreadyRecorded: "{type} déjà enregistrée",
+      created: "{type} créée",
+      updated: "{type} mise à jour",
+      deleted: "{type} supprimée",
+    },
+  },
 } as const;
