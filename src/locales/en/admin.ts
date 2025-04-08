d75adb5cb260f99f6ab485f3cff6111c8dfc04d2
Julien Hauseux <julien.hauseux@gmail.com>
2025-04-08 10:09:43 +0200

    WIP -- Requests/priorities

--- src/locales/en/admin.ts
+++ src/locales/en/admin.ts
@@ -690,13 +690,146 @@ If these types are assigned to courses, you will not be able to delete them.`,
         },
       },
     },
+
     requests: {
-      title: "Requests and priorities",
+      title: "Demandes et priorités",
       requests: {
-        label: "Requests",
+        label: "Demandes",
+        table: {
+          columns: {
+            year: "Année",
+            type: "Type demande",
+            hours: "H.",
+            uid: "Intervenant",
+            degree: "Diplôme",
+            program: "Mention",
+            track: "Parcours",
+            course: "Enseignement",
+            semester: "S.",
+            courseType: "Type ens.",
+          },
+        },
+        form: {
+          title: {
+            none: "Nouvelle demande",
+            single: "{label}",
+            multiple: "{count} demandes sélectionnés",
+          },
+          fields: {
+            year: "Année",
+            type: "Type de demande",
+            hours: "Nombre d'heures demandées",
+            uid: "Intervenant",
+            degree: "Diplôme",
+            program: "Mention",
+            track: "Parcours",
+            course: "Enseignement",
+            semester: "Semestre",
+            courseType: "Type d'enseignement",
+          },
+          error: {
+            updateUidWithoutYear:
+              "Vous ne pouvez pas modifier l'intervenant sans sélectionner une année'",
+            serviceNotFound:
+              "Il n'existe pas de service pour l'intervenant {uid} pour l'année {year}",
+            updateCourseMissingFields:
+              "Pour mettre à jour le cours, vous devez sélectionner une année, un diplôme, une mention, un parcours (éventuellement vide), un enseignement, un semestre et un type d'enseignement",
+            courseNotFound:
+              "Il n'existe pas de {type} au semestre {semester} avec le nom « {name} » dans le parcours « {track} » de la mention « {program} » du diplôme « {degree} » pour l'année {year}",
+          },
+        },
+        data: {
+          success: {
+            insert:
+              "Aucune demande créée | Demande créée | {count} demandes créées",
+            update:
+              "Aucune demande mise à jour | Demande mise à jour | {count} demandes mises à jour",
+            delete:
+              "Aucune demande supprimée | Demande supprimée | {count} demandes supprimées",
+            import:
+              "0 demande importée | 1 demande importée | {count} demandes importées",
+            export:
+              "0 demande exportée | 1 demande exportée | {count} demandes exportées",
+          },
+          confirm: {
+            delete: {
+              single:
+                "Êtes-vous sûr de vouloir supprimer la demande « {label} » ?",
+              multiple:
+                "Êtes-vous sûr de vouloir supprimer les {count} demandes sélectionnées ?",
+            },
+          },
+        },
       },
       priorities: {
-        label: "Priorities",
+        label: "Priorités",
+        table: {
+          columns: {
+            year: "Année",
+            seniority: "Ancienneté",
+            isPriority: "Prioritaire",
+            computed: "Calculée",
+            uid: "Intervenant",
+            degree: "Diplôme",
+            program: "Mention",
+            track: "Parcours",
+            course: "Enseignement",
+            semester: "S.",
+            courseType: "Type ens.",
+          },
+        },
+        form: {
+          title: {
+            none: "Nouvelle priorité",
+            single: "{label}",
+            multiple: "{count} priorités sélectionnés",
+          },
+          fields: {
+            year: "Année",
+            seniority: "Ancienneté",
+            isPriority: "Prioritaire",
+            computed: "Calculée",
+            uid: "Intervenant",
+            degree: "Diplôme",
+            program: "Mention",
+            track: "Parcours",
+            course: "Enseignement",
+            semester: "Semestre",
+            courseType: "Type d'enseignement",
+          },
+          error: {
+            updateUidWithoutYear:
+              "Vous ne pouvez pas modifier l'intervenant sans sélectionner une année'",
+            serviceNotFound:
+              "Il n'existe pas de service pour l'intervenant {uid} pour l'année {year}",
+            updateCourseMissingFields:
+              "Pour mettre à jour le cours, vous devez sélectionner une année, un diplôme, une mention, un parcours (éventuellement vide), un enseignement, un semestre et un type d'enseignement",
+            courseNotFound:
+              "Il n'existe pas de {type} au semestre {semester} avec le nom « {name} » dans le parcours « {track} » de la mention « {program} » du diplôme « {degree} » pour l'année {year}",
+          },
+        },
+        data: {
+          success: {
+            insert:
+              "Aucune priorité créée | Priorité créée | {count} priorités créées",
+            update:
+              "Aucune priorité mise à jour | Priorité mise à jour | {count} priorités mises à jour",
+            delete:
+              "Aucune priorité supprimée | Priorité supprimée | {count} priorités supprimées",
+            import:
+              "0 priorité importée | 1 priorité importée | {count} priorités importées",
+            export:
+              "0 priorité exportée | 1 priorité exportée | {count} priorités exportées",
+          },
+          confirm: {
+            delete: {
+              single:
+                "Êtes-vous sûr de vouloir supprimer la priorité « {label} » ?",
+              multiple:
+                "Êtes-vous sûr de vouloir supprimer les {count} priorités sélectionnées ?",
+            },
+          },
+        },
       },
     },
     roles: {
