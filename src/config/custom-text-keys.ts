007ea02746c07c9c06daed9db0c5126520d764d7
Julien Hauseux <julien.hauseux@gmail.com>
2025-04-16 02:13:45 +0200

    WIP -- fix with isCustomTextKey

--- src/config/custom-text-keys.ts
+++ src/config/custom-text-keys.ts
@@ -18,4 +18,4 @@ export const CUSTOM_TEXT_KEYS = [
 export type CustomTextKey = (typeof CUSTOM_TEXT_KEYS)[number];
 
 export const isCustomTextKey = (key: unknown): key is CustomTextKey =>
-  Object.values(CUSTOM_TEXT_KEYS).includes(key as CustomTextKey);
+  CUSTOM_TEXT_KEYS.includes(key as CustomTextKey);
